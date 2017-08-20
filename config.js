0b23e995e4bf972f63e996b34df934bae13a24c6
Chun-MingChen <nick830314@gmail.com>
2017-08-20 23:45:59 +0800

    minify and autoprefix index.css with gulp

---
+++ config.js
@@ -0,0 +1,9 @@
+import path from 'path';
+
+export default {
+  destination: path.join(__dirname, 'dist'),
+  cssBasePath: path.join(__dirname, 'src', 'assets', 'stylesheet'),
+  renameSetting: {
+    suffix: '.min',
+  },
+};
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,14 @@
 import gulp from 'gulp';
 import fetch from 'node-fetch';
 import fs from 'fs';
+import path from 'path';
+import runSequence from 'run-sequence';
+import autoprefixer from 'autoprefixer';
+import postcss from 'gulp-postcss';
+import cssnano from 'gulp-cssnano';
+import rename from 'gulp-rename';
 import pageConfig from './page.config';
+import config from './config';
 
 gulp.task('fetch:website', () => {
   const { targetPage, destPath } = pageConfig;
@@ -20,4 +27,34 @@ gulp.task('fetch:website', () => {
       console.log('\x1b[31m%s\x1b[0m', 'fetching content failed :(');
       console.error(error);
     });
+});
+
+
+gulp.task('css:generate', function() {
+  // there are two sub-task to do
+  // 1. generate css
+  runSequence('css:minify');
+});
+
+/* This part is to minify css
+ * what we do is
+ * 1. auto-prefix
+ * 2. minify
+ * 3. generate file into dist
+ */
+gulp.task('css:minify', function() {
+  const { cssBasePath, renameSetting, destination } = config;
+  const plugins = [
+    autoprefixer({
+      browsers: ['> 1%'],
+    }),
+  ];
+
+  console.log('\x1b[32m%s\x1b[0m', 'now start minifying css :)');
+
+  return gulp.src(path.join(cssBasePath, 'index.css'))
+    .pipe(postcss(plugins))
+    .pipe(cssnano())
+    .pipe(rename(renameSetting))
+    .pipe(gulp.dest(destination));
 });
